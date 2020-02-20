d9538d0bffc3e8e842e9d1a4725828f8a67d0c5f
rd <rd@rd-desktop>
2020-02-20 12:53:13 +0100

    1st app version

---
+++ src/pl/Majster/EqApp/panele/StatyV2.java
@@ -0,0 +1,40 @@
+package pl.Majster.EqApp.panele;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
+public class StatyV2 extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	JTable tabela;
+	
+	String[] kolumny = {"Range","Raw EQ", "Q(45)","Q(65)","Comp Flop EQ"};
+	
+	public StatyV2() {
+	
+		
+		String[][] dane = {
+				
+				{"50%","50", "60", "70","80"}
+		};
+		
+		
+		
+		tabela = new JTable(dane, kolumny);
+		tabela.setBounds(38, 12, 375, 138);
+		add(tabela.getTableHeader());
+		add(tabela);
+		
+		
+		
+		
+		
+		
+	}
+
+	
+	
+}
